5b104d9b370bb64045fe33452904ba7c67595d6b
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-08-07 15:57:17 +0300

    create dummy secret value vol2

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -8,8 +8,19 @@ resource "google_secret_manager_secret" "secret" {
   }
   version_destroy_ttl = "86400s"
 }
+
+resource "google_secret_manager_secret_version" "initial" {
+  secret      = google_secret_manager_secret.secret.id
+  secret_data = "{}"
+  
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
 data "google_secret_manager_secret_version" "secret_version_actual" {
-  secret = google_secret_manager_secret.secret.id
+  secret     = google_secret_manager_secret.secret.id
+  depends_on = [google_secret_manager_secret_version.initial]
 }
 output "secret_output_dict" {
   sensitive = true
