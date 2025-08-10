46a9897e06cd2484b592d562fef40245f10063d5
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2025-08-11 00:45:48 +0300

    try to fix secrets setup for good

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -9,19 +9,22 @@ resource "google_secret_manager_secret" "secret" {
   version_destroy_ttl = "86400s"
 }
 
-resource "google_secret_manager_secret_version" "initial" {
-  secret      = google_secret_manager_secret.secret.id
-  secret_data = "{}"
+data "google_secret_manager_secret_version" "bootstrapped" {
+  secret            = google_secret_manager_secret.secret.id
+  version           = "1"
+  fetch_secret_data = false
+}
 
-  lifecycle {
-    ignore_changes = all
-  }
+resource "google_secret_manager_secret_version" "initial" {
+  count          = can(data.google_secret_manager_secret_version.bootstrapped) ? 0 : 1
+  secret         = google_secret_manager_secret.secret.id
+  secret_data_wo = "{}"
 }
 
 data "google_secret_manager_secret_version" "secret_version_actual" {
-  secret     = google_secret_manager_secret.secret.id
-  depends_on = [google_secret_manager_secret_version.initial]
+  secret = google_secret_manager_secret.secret.id
 }
+
 output "secret_output_dict" {
   sensitive = true
   value     = jsondecode(data.google_secret_manager_secret_version.secret_version_actual.secret_data)
