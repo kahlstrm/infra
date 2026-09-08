f69e09d2bf453ba4407aa7bf2c62a93020c7d283
Kalle Ahlström <kalle.ahlstrom@iki.fi>
2026-09-09 02:57:50 +0300

    test: preserve DNS comments when forcing changed IDs

--- experiments/chr/scenarios/bootstrap/__init__.py
+++ experiments/chr/scenarios/bootstrap/__init__.py
@@ -415,7 +415,7 @@ def experiment(parent):
             list(executor.map(lambda router: router.reset_bootstrap(), routers))
         for router in routers:
             router.ssh(
-                ":foreach id in=[/ip dns static find] do={:local n [/ip dns static get $id name]; :local t [/ip dns static get $id type]; :local a [/ip dns static get $id address]; :local d [/ip dns static get $id disabled]; /ip dns static remove $id; /ip dns static add name=$n type=$t address=$a disabled=$d}"
+                ":foreach id in=[/ip dns static find] do={:local n [/ip dns static get $id name]; :local t [/ip dns static get $id type]; :local a [/ip dns static get $id address]; :local d [/ip dns static get $id disabled]; :local c [/ip dns static get $id comment]; /ip dns static remove $id; /ip dns static add name=$n type=$t address=$a disabled=$d comment=$c}"
             )
         adoption.adopt(recovery=True)
         for router in routers:
